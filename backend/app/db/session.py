b1b1ed605fe21e049a6dce2f8b44c684867aa037
Sergiy Brovchenko <serguz@ukr.net>
2025-05-31 19:00:49 +0300

    Alembic set up

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,5 +2,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.database_url)
+engine = create_engine(settings.database_url, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
