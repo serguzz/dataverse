3e8efeb74be33c3f9ce55722371d144da359561b
Sergiy Brovchenko <serguz@ukr.net>
2025-05-31 18:34:54 +0300

    Basic Alembic setup

---
+++ backend/app/db/models/dataset.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
+
+class Dataset(Base):
+    __tablename__ = "datasets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
